2d9cb6743e491c8471e83a604ce04a533c7474ac
Jason Cardenas <jasonc62492@gmail.com>
2024-04-06 15:39:19 -0400

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,29 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { OpenAiService } from "./open-ai.service";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from "@angular/fire/analytics";
+import {
+  initializeAppCheck,
+  ReCaptchaEnterpriseProvider,
+  provideAppCheck,
+} from "@angular/fire/app-check";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { getDatabase, provideDatabase } from "@angular/fire/database";
+import { getFunctions, provideFunctions } from "@angular/fire/functions";
+import { getMessaging, provideMessaging } from "@angular/fire/messaging";
+import { getPerformance, providePerformance } from "@angular/fire/performance";
+import { getStorage, provideStorage } from "@angular/fire/storage";
+import {
+  getRemoteConfig,
+  provideRemoteConfig,
+} from "@angular/fire/remote-config";
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,8 +38,33 @@ import { OpenAiService } from "./open-ai.service";
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: "dj-u-b5c03",
+        appId: "1:370462437523:web:b917a70f21c36096c45d66",
+        storageBucket: "dj-u-b5c03.appspot.com",
+        apiKey: "AIzaSyCyo1k3YhbCiyXKH5nRaYKmeVhNHz1BC34",
+        authDomain: "dj-u-b5c03.firebaseapp.com",
+        messagingSenderId: "370462437523",
+        measurementId: "G-XS5FT3TTFG",
+      }),
+    ),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    // provideAppCheck(() => {
+    // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+    //   const provider = new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
+    //   return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+    // }),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideStorage(() => getStorage()),
+    provideRemoteConfig(() => getRemoteConfig()),
   ],
-  providers: [OpenAiService],
+  providers: [OpenAiService, ScreenTrackingService, UserTrackingService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
